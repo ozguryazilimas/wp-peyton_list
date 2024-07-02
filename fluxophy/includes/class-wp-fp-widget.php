072090673e41cd77577e344388df6f807cb274bc
Onur Küçük <onur@delipenguen.net>
2024-07-02 15:38:33 +0300

    remove facebook widget

--- fluxophy/includes/class-wp-fp-widget.php
+++ fluxophy/includes/class-wp-fp-widget.php
@@ -148,25 +148,30 @@ function fluxophy_display_data($source_url, $display_count, $picture_url, $accou
    */
 
   $output .= '<div>';
-  $output .= '<div id="fluxophy_button_show_twitter">';
+  # $output .= '<div id="fluxophy_button_show_twitter">';
+  $output .= '<div id="fluxophy_button_show_twitter" class="active">';
   $output .= '<span class="fluxophy_button_home fluxophy_button_home_twitter">';
   $output .= '<svg viewBox="0 0 512 512" width="25" height="25" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M419.6 168.6c-11.7 5.2-24.2 8.7-37.4 10.2 13.4-8.1 23.8-20.8 28.6-36 -12.6 7.5-26.5 12.9-41.3 15.8 -11.9-12.6-28.8-20.6-47.5-20.6 -42 0-72.9 39.2-63.4 79.9 -54.1-2.7-102.1-28.6-134.2-68 -17 29.2-8.8 67.5 20.1 86.9 -10.7-0.3-20.7-3.3-29.5-8.1 -0.7 30.2 20.9 58.4 52.2 64.6 -9.2 2.5-19.2 3.1-29.4 1.1 8.3 25.9 32.3 44.7 60.8 45.2 -27.4 21.4-61.8 31-96.4 27 28.8 18.5 63 29.2 99.8 29.2 120.8 0 189.1-102.1 185-193.6C399.9 193.1 410.9 181.7 419.6 168.6z"></path></g></svg>';
   $output .= '</span>';
   $output .= '</div>';
-  $output .= '<div id="fluxophy_button_show_fb" class="active">';
-  $output .= '<span class="fluxophy_button_home fluxophy_button_home_fb">';
-  $output .= '<svg viewBox="0 0 33 33" width="25" height="25" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M 17.996,32L 12,32 L 12,16 l-4,0 l0-5.514 l 4-0.002l-0.006-3.248C 11.993,2.737, 13.213,0, 18.512,0l 4.412,0 l0,5.515 l-2.757,0 c-2.063,0-2.163,0.77-2.163,2.209l-0.008,2.76l 4.959,0 l-0.585,5.514L 18,16L 17.996,32z"></path></g></svg>';
-  $output .= '</span>';
-  $output .= '</div>';
-  $output .= '</div>';
 
-  $output .= '<div id="fluxophy_widget_fb">';
-  $output .= '<ul id="fluxophy_widget">';
-  $output .= fluxophy_fetch_data_fb($source_url, $display_count, $picture_url);
-  $output .= '</ul>';
-  $output .= '</div>';
+  # $output .= '<br>';
 
-  $output .= '<div id="fluxophy_widget_twitter" style="display:none;">';
+  # $output .= '<div id="fluxophy_button_show_fb" class="active">';
+  # $output .= '<span class="fluxophy_button_home fluxophy_button_home_fb">';
+  # $output .= '<svg viewBox="0 0 33 33" width="25" height="25" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M 17.996,32L 12,32 L 12,16 l-4,0 l0-5.514 l 4-0.002l-0.006-3.248C 11.993,2.737, 13.213,0, 18.512,0l 4.412,0 l0,5.515 l-2.757,0 c-2.063,0-2.163,0.77-2.163,2.209l-0.008,2.76l 4.959,0 l-0.585,5.514L 18,16L 17.996,32z"></path></g></svg>';
+  # $output .= '</span>';
+  # $output .= '</div>';
+  # $output .= '</div>';
+
+  # $output .= '<div id="fluxophy_widget_fb">';
+  # $output .= '<ul id="fluxophy_widget">';
+  # $output .= fluxophy_fetch_data_fb($source_url, $display_count, $picture_url);
+  # $output .= '</ul>';
+  # $output .= '</div>';
+
+  # $output .= '<div id="fluxophy_widget_twitter" style="display:none;">';
+  $output .= '<div id="fluxophy_widget_twitter" style="">';
   $output .= fluxophy_fetch_data_twitter($twitter_account, $twitter_data_id);
   $output .= '</div>';
 
