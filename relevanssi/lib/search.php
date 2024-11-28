115830c4aa8a5daf3ab9e7162bbe56e9fddb3b94
Onur Küçük <onur@delipenguen.net>
2024-11-28 23:30:12 +0300

    refs #82641 update relevanssi from 4.23.1 to 4.24.0

--- relevanssi/lib/search.php
+++ relevanssi/lib/search.php
@@ -661,7 +661,8 @@ function relevanssi_do_query( &$query ) {
 			$q,
 			$query
 		);
-		relevanssi_update_log( $query_string, $hits_count );
+		$source = $query->query_vars['rlv_source'] ?? '';
+		relevanssi_update_log( $query_string, $hits_count, $source );
 	}
 
 	$make_excerpts = 'on' === get_option( 'relevanssi_excerpts' ) ? true : false;
@@ -1752,14 +1753,14 @@ function relevanssi_compile_common_args( $query ) {
 	$date_query = relevanssi_wp_date_query_from_query_vars( $query );
 
 	$post_type = false;
-	if ( isset( $query->query_vars['post_type'] ) && is_array( $query->query_vars['post_type'] ) ) {
-		$query->query_vars['post_type'] = implode( ',', $query->query_vars['post_type'] );
-	}
 	if ( isset( $query->query_vars['post_type'] ) && 'any' !== $query->query_vars['post_type'] ) {
 		$post_type = $query->query_vars['post_type'];
 	}
 	if ( isset( $query->query_vars['post_types'] ) && 'any' !== $query->query_vars['post_types'] ) {
 		$post_type = $query->query_vars['post_types'];
+	}
+	if ( is_array( $post_type ) ) {
+		$post_type = implode( ',', $post_type );
 	}
 
 	$post_status = false;
